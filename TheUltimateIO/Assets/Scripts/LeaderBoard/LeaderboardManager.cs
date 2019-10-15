0d252f44d84b7f1319fe3e95f4aad14ff8b242fd
Chiqquii <47392436+Chiqquii@users.noreply.github.com>
2019-10-15 15:27:07 -0300

    correccion test

--- TheUltimateIO/Assets/Scripts/LeaderBoard/LeaderboardManager.cs
+++ TheUltimateIO/Assets/Scripts/LeaderBoard/LeaderboardManager.cs
@@ -20,7 +20,6 @@ namespace Leaderboard
 
         public void UpdateUserPoints(string newUserNickname, int addedPoints)
         {
-            Debug.LogError("ASDASDASDA   "+ addedPoints);
             if(addedPoints == REMOVE) //si es igual a 404 significa que entonces se fue de la partida
             {
                 allUserData.Remove(newUserNickname);
