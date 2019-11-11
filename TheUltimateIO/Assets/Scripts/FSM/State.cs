b5dfc1f00d067cb1e7d7c6a18d414c67b4193efd
Chiqquii <47392436+Chiqquii@users.noreply.github.com>
2019-11-11 20:51:35 -0300

    FSM

--- TheUltimateIO/Assets/Scripts/FSM/State.cs
+++ TheUltimateIO/Assets/Scripts/FSM/State.cs
@@ -1,63 +1,56 @@
 using System;
 using System.Collections.Generic;
 
-namespace IA2
+namespace FSM
 {
-	public class State<T>
+    public class State<T>
     {
-		public string Name { get { return _stateName; } }
-
-		public event Action<T> OnEnter = delegate {};
-		public event Action OnUpdate = delegate {};
-        public event Action OnLateUpdate = delegate { };
+        public event Action<T> OnEnter = delegate { };
+        public event Action OnUpdate = delegate { };
         public event Action OnFixedUpdate = delegate { };
-		public event Action<T> OnExit = delegate {};
+        public event Action<T> OnExit = delegate { };
 
-		private string _stateName;
-		private Dictionary<T, Transition<T>> transitions;
+        private string _stateName;
+        private Dictionary<T, Transition<T>> transitions;
 
-		public State(string name)
+        public State(string name)
         {
-			_stateName = name;
-		}
+            _stateName = name;
+        }
 
-		public State<T> Configure(Dictionary<T, Transition<T>> transitions)
+        public State<T> Configure(Dictionary<T, Transition<T>> transitions)
         {
-			this.transitions = transitions;
-			return this;
-		}
+            this.transitions = transitions;
+            return this;
+        }
 
-		public Transition<T> GetTransition(T input)
+        public Transition<T> GetTransition(T input)
         {
-			return transitions[input];
-	    }
+            return transitions[input];
+        }
 
-		public bool CheckInput(T input, out State<T> next)
+        public bool CheckInput(T input, out State<T> next)
         {
-			if(transitions.ContainsKey(input)) {
-				var transition = transitions[input];
-				transition.OnTransitionExecute(input);
-				next = transition.TargetState;
-				return true;
-			}
+            if (transitions.ContainsKey(input))
+            {
+                var transition = transitions[input];
+                transition.OnTransitionExecute(input);
+                next = transition.targetState;
+                return true;
+            }
 
-			next = this;
-			return false;
-		}
+            next = this;
+            return false;
+        }
 
-		public void Enter(T input)
+        public void Enter(T input)
         {
-			OnEnter(input);
-		}
+            OnEnter(input);
+        }
 
-		public void Update()
+        public void Update()
         {
-			OnUpdate();
-		}
-
-        public void LateUpdate()
-        {
-            OnLateUpdate();
+            OnUpdate();
         }
 
         public void FixedUpdate()
@@ -65,9 +58,9 @@ namespace IA2
             OnFixedUpdate();
         }
 
-		public void Exit(T input)
+        public void Exit(T input)
         {
-			OnExit(input);
-		}
-	}
+            OnExit(input);
+        }
+    }
 }
