b268c1ef9df5f0e623fe85d926959e6459f9b602
Chiqquii <47392436+Chiqquii@users.noreply.github.com>
2019-11-26 16:00:23 -0300

    Minimo de jugadores
    Se agrego que tengan que ser minimo 2 jugadores para podes empezar la partida

--- TheUltimateIO/Assets/Scripts/GameManagers/NetManager.cs
+++ TheUltimateIO/Assets/Scripts/GameManagers/NetManager.cs
@@ -2,6 +2,8 @@
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
 
 public class NetManager : MonoBehaviourPunCallbacks
 {
@@ -9,11 +11,16 @@ public class NetManager : MonoBehaviourPunCallbacks
     public string gameVersion = "0.0.1";
     public int sceneID;
 
+    public int minPlayersStartGame;
+    public GameObject canvasWaitingStart;
+    public TextMeshProUGUI textNamesPlayers, textWaiting;
+
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
 
         PhotonNetwork.GameVersion = gameVersion;
+        textWaiting.text = "There must be at least " + minPlayersStartGame + " players to start the game...";
     }
 
     public void Connect(int newSceneID)
@@ -37,13 +44,21 @@ public class NetManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log(PhotonNetwork.LocalPlayer.NickName + " has entered the room :" + PhotonNetwork.CurrentRoom + "!");
-        PhotonNetwork.LoadLevel(sceneID);
+
         if (PhotonNetwork.CurrentRoom.PlayerCount <= 1) //es decir que somos el que creo el room
         {
-            var go = PhotonNetwork.Instantiate("LevelManager", transform.position, Quaternion.identity);
-            DontDestroyOnLoad(go);
+             var go = PhotonNetwork.Instantiate("LevelManager", transform.position, Quaternion.identity);
+             DontDestroyOnLoad(go);
+        }
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount < minPlayersStartGame)
+            StartCoroutine(WaitingStart());
+        else
+        {
+            PhotonNetwork.LoadLevel(sceneID);
             SceneManager.sceneLoaded += OnSceneLoaded;
         }
+
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -57,4 +72,31 @@ public class NetManager : MonoBehaviourPunCallbacks
         Debug.LogWarning(returnCode + ": " + message);
         PhotonNetwork.CreateRoom(Random.Range(0, 9999).ToString(), new RoomOptions() { MaxPlayers = 19 });
     }
+
+    IEnumerator WaitingStart()
+    {
+        var waitForEndOfFrame = new WaitForEndOfFrame();
+        canvasWaitingStart.SetActive(true);
+        while (PhotonNetwork.CurrentRoom.PlayerCount < minPlayersStartGame)
+        {
+            Debug.Log("Waiting players...");
+            yield return waitForEndOfFrame;
+        }
+        canvasWaitingStart.SetActive(false);
+        PhotonNetwork.LoadLevel(sceneID);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    /*
+    [PunRPC]
+    void AddNameWaiting(string name)
+    {
+        textNamesPlayers.text += " - " + name;
+    }
+
+    [PunRPC]
+    void FirstAddNameWaiting(string name)
+    {
+        textNamesPlayers.text += name;
+    }
+    */
 }
