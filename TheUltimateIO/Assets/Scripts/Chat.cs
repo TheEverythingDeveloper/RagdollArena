8f1b9012151ede30350e9d3cd6dc62d7a2b35345
Chiqquii <47392436+Chiqquii@users.noreply.github.com>
2020-01-08 20:14:07 -0300

    Chat

--- TheUltimateIO/Assets/Scripts/Chat.cs
+++ TheUltimateIO/Assets/Scripts/Chat.cs
@@ -4,17 +4,18 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class Chat : MonoBehaviourPun
 {
+    public GameObject chatArea;
     public Text msgInput;
-    public Text msgArea;
     Dictionary<int, string> _typesMsg = new Dictionary<int, string>();
     public Dropdown typeMsg;
+    public GameObject content;
+    public TextMeshProUGUI textMsg;
 
     void Start()
     {
-        //        if (!photonView.IsMine) return;
         _typesMsg.Add(0, "RPCGlobalSendMsg");
         _typesMsg.Add(1, "RPCTeamSendMsg");
         _typesMsg.Add(2, "RPCPrivateSendMsg");
@@ -35,19 +36,32 @@ public class Chat : MonoBehaviourPun
     [PunRPC]
     void RPCGlobalSendMsg(string msg, string name)
     {
-        msgArea.text += "<color=green> |GLOBAL| </color>" + name + ": " + msg;
+        var newText = Instantiate(textMsg);
+        newText.gameObject.SetActive(true);
+        newText.text = "<color=green> |GLOBAL| </color>" + name + ": " + msg;
+        newText.transform.parent = content.transform;
     }
 
     [PunRPC]
     void RPCTeamSendMsg(string msg, string name)
     {
-        msgArea.text += "<color=red> |TEAM| </color>" + name + ": " + msg;
+        var newText = Instantiate(textMsg);
+        newText.gameObject.SetActive(true);
+        newText.text = "<color=red> |TEAM| </color>" + name + ": " + msg;
+        newText.transform.parent = content.transform;
     }
 
     [PunRPC]
     void RPCPrivateSendMsg(string msg, string name)
     {
-        msgArea.text += "<color=blue> |PRIVATE| </color>" + name + ": " + msg;
+        var newText = Instantiate(textMsg);
+        newText.gameObject.SetActive(true);
+        newText.text = "<color=blue> |PRIVATE| </color>" + name + ": " + msg;
+        newText.transform.parent = content.transform;
     }
 
+    public void OpenOrCloseChat(bool open)
+    {
+        chatArea.SetActive(open);
+    }
 }
